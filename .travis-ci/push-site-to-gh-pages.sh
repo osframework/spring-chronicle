d67a78bcaa869d88f27c5d9db539470f2f7e6168
Dave Joyce <dave@osframework.org>
2015-06-30 23:11:27 -0400

    #6 Fixing published site contents, target directories

--- .travis-ci/push-site-to-gh-pages.sh
+++ .travis-ci/push-site-to-gh-pages.sh
@@ -3,12 +3,14 @@
 if [ "$TRAVIS_REPO_SLUG" == "osframework/spring-chronicle" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	echo -e "Publishing site...\n"
 
-	cp -R target/site $TRAVIS_BUILD_DIR/site-latest
+	cp -R target/site $HOME/site-latest
 
-	cd $TRAVIS_BUILD_DIR
+	cd $HOME
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "travis-ci"
 	git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/osframework/spring-chronicle gh-pages > /dev/null
+
+	echo -e "Cloned branch 'gh-pages' to ${HOME}/gh-pages\n"
 
 	cd gh-pages
 	git rm -rf ./apidocs
@@ -16,7 +18,10 @@ if [ "$TRAVIS_REPO_SLUG" == "osframework/spring-chronicle" ] && [ "$TRAVIS_PULL_
 	git rm -rf ./images
 	git rm -rf ./testapidocs
 	git rm -rf ./*.html
-	cp -Rf $TRAVIS_BUILD_DIR/site-latest ./
+	cp -Rf $HOME/site-latest/apidocs ./apidocs
+	cp -Rf $HOME/site-latest/css ./css
+	cp -Rf $HOME/site-latest/images ./images
+	cp $HOME/site-latest/*.html ./
 	git add -f .
 	git commit -m "Latest site generated on successful build $TRAVIS_BUILD_NUMBER"
 	git push -fq origin gh-pages > /dev/null
